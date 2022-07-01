860aff1d1bca7b3af194fd188b041544279331e5
xu chen <xuchen@xus-macbook-pro.lan>
2022-06-30 20:06:17 -0700

    inclass demo

---
+++ exp_demo3b.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+var routedemo = require("./exp_demo3a.js");
+
+
+app.use("/routedemo", routedemo);
+//both exp-demo3a.js and exp-demo3b.js are in same directory
+
+
+app.listen(3000);
+
+
